473824719fa27aa9ca43162617e695a0b44bfb78
stephendjeumo-rgb <stephen.djeumo@2030.ucac-icam.com>
2026-08-27 04:50:04 +0200

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -100,7 +100,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap",
       },
-      { rel: "icon", type: "image/png", href: "/favicon.png" },
+      { rel: "icon", type: "image/png", href: "/favicon2.png" },
     ],
   }),
 
